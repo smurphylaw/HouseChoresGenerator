01f5eb480dacdb4f6e36ece1f918cf66474c5934
Stephen Murphy <smurphylaw@gmail.com>
2018-06-22 14:15:09 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,43 @@
+/*  
+    House Chores Generator Project  
+*/
+
+var express = require('express');
+var mysql = require('./dbcon.js');
+var bodyParser = require('body-parser');
+
+var app = express();
+var handlebars = require('express-handlebars').create({
+  defaultLayout:'main'
+});
+
+app.engine('handlebars', handlebars.engine);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/static', express.static('public'));
+app.set('view engine', 'handlebars');
+app.set('port', process.argv[2]);
+app.set('mysql', mysql);
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+app.use('/generator', require('./generator.js'));
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
